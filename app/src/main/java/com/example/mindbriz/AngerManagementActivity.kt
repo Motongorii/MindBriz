d36a4194c3f8dff1280881dbba808d08c01b29a1
TeckMoto <antomotongorii@gmail.com>
2023-06-19 17:07:21 +0300

    from mindbriz

--- app/src/main/java/com/example/mindbriz/AngerManagementActivity.kt
+++ app/src/main/java/com/example/mindbriz/AngerManagementActivity.kt
@@ -3,9 +3,9 @@ package com.example.mindbriz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AngerManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_anger_management)
     }
 }
--- app/src/main/java/com/example/mindbriz/SelfHarmMngActivity.kt
+++ app/src/main/java/com/example/mindbriz/SelfHarmMngActivity.kt
@@ -3,9 +3,9 @@ package com.example.mindbriz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SelfHarmMngActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_self_harm_mng)
     }
 }
